b15c33fc65c5fb2e9f55ccbd9f6c8a9086978d26
Aditya Kumar Pandey <100374491+aditya343117@users.noreply.github.com>
2022-10-19 09:20:09 +0530

    Time: 5 ms (13.90%), Space: 6 MB (66.13%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        
+        int i,j;
+        
+        for(i=s.size()-2; i>=0; i--){
+            if(s[i]<s[i+1]) break;
+        }
+        
+        if(i<0) return -1;
+        
+        for(j=s.size()-1; j>=0; j--){
+            if(s[j]>s[i]) break;
+        }
+        swap(s[i], s[j]);
+        reverse(s.begin()+i+1, s.end());
+        
+        int ans=stoll(s);
+        if(ans>n) return ans;
+        return -1;
+    }
+};
