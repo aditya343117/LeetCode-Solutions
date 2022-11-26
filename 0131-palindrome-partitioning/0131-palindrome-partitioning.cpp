806751caf414d97e17514d25b35dbfbbd479293f
Aditya Kumar Pandey <100374491+aditya343117@users.noreply.github.com>
2022-11-26 10:44:40 +0530

    Time: 355 ms (23.12%), Space: 239.1 MB (5.11%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    
+    bool palindrom(string s){
+        int i=0;
+        int j=s.size()-1;
+        
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    void solve(vector<string> op, string ip){
+        
+        if(ip.size()==0){
+            ans.push_back(op);
+            return;
+        }
+        
+        
+        for(int i=0; i<ip.size(); i++){
+            string t=ip.substr(0, i+1);
+            
+            if(palindrom(t)){
+               vector<string>op1=op;
+               op1.push_back(t);
+               string ip1=ip.substr(i+1);
+                
+               solve(op1, ip1); 
+            }
+        }
+        
+    }
+    
+    vector<vector<string>> partition(string s) {
+        string ip=s;
+        vector<string>op;
+        
+        solve(op, ip);
+        
+        return ans;
+    }
+};
